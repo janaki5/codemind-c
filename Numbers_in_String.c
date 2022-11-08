0083d49c1a4fae30cb270a6003492309327895f3
JANAKI VELAMALA <102218875+janaki5@users.noreply.github.com>
2022-11-08 10:12:51 +0530

    #include<stdio.h> #include<string.h> int main() {     char s[1000];     scanf("%[^ ]s",s);     int c = 0;     for(int i=0; i<strlen(s); i++)     {         if((int)s[i] >= 48 && (int)s[i] <= 57)             c += s[i]-'0';     }     printf("%d",c); }

---
+++ Numbers_in_String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[1000];
+    scanf("%[^
+]s",s);
+    int c = 0;
+    for(int i=0; i<strlen(s); i++)
+    {
+        if((int)s[i] >= 48 && (int)s[i] <= 57)
+            c += s[i]-'0';
+    }
+    printf("%d",c);
+}
